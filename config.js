fb87965ceeaee07beddbfe3a8f0a6c2dd165144b
kailin <pooc95@163.com>
2021-04-29 09:43:46 +0800

    test

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const MO_FISH_PASSWORD = process.env.MO_FISH_PASSWORD
 // server酱推送
 const SERVER_J = process.env.SERVER_J
 
-console.log(MO_FISH_USERNAME, MO_FISH_PASSWORD, SERVER_J);
+console.log(MO_FISH_USERNAME.length, MO_FISH_PASSWORD.length, SERVER_J.length);
 
 module.exports = {
   MO_FISH_USERNAME,
