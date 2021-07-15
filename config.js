b83d0feeb45923f50b0dc726222601f29e6aa451
kailin <pooc95@163.com>
2021-07-15 10:19:40 +0800

    add: 修改 config

--- config.js
+++ config.js
@@ -4,11 +4,15 @@
 const MO_FISH_USERNAME = process.env.MO_FISH_USERNAME
 const MO_FISH_PASSWORD = process.env.MO_FISH_PASSWORD
 
+// JueJin Cookie
+const JUE_JIN_COOKIE = process.env.JUE_JIN_COOKIE
+
 // server酱推送
 const SERVER_J = process.env.SERVER_J
 
 module.exports = {
   MO_FISH_USERNAME,
   MO_FISH_PASSWORD,
+  JUE_JIN_COOKIE,
   SERVER_J,
 }
